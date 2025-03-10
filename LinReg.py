87a2ce12a72fe100ea3a238c53d19b0784234d1c
cobalte_analytics <69243075+cobalte90@users.noreply.github.com>
2025-03-10 12:59:48 +0300

    Update LinReg.py

--- LinReg.py
+++ LinReg.py
@@ -14,11 +14,6 @@ class LinReg:
     def func(self, X, w, b):  # Вычисление таргетов по фичам, весам и смещению
         return X.dot(w) + b
 
-    def MSE(self):
-        y_pred = self.func(self.X, self.w, self.b)
-        mse = np.mean((self.y - y_pred) ** 2)
-        return mse
-
     def fit(self, learning_rate):
         X = self.X # Фичи
         y = self.y # Таргет
@@ -31,12 +26,20 @@ class LinReg:
             y_pred = self.func(X, self.w, self.b)
             self.b -= learning_rate * (2 / len(X)) * np.sum(y_pred - y_true)
 
-            for i in range(len(self.w)):
-                self.w[i] -= learning_rate * (2 / len(X)) * np.sum((y_pred - y_true) * X.iloc[:, i])
+            error = y_pred - y_true
+            grad = (2 / len(X)) * np.dot(X.T, error)
+            self.w -= learning_rate * grad
 
 
         return self.func(X, self.w, self.b)
 
+
+    def predict(self, X_test):
+        return self.func(X_test, self.w, self.b)
+
+def MAE(y_true, y_pred):
+    mae = np.mean(abs(y_true - y_pred))
+    return mae
 
     def predict(self, X_test):
         return self.func(X_test, self.w, self.b)
