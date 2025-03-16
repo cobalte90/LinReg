90cf8fe53b7f5d8b94553e1b7e7fe4b8cd2e7330
cobalte_analytics <69243075+cobalte90@users.noreply.github.com>
2025-03-16 16:58:14 +0300

    Add files via upload

--- LinReg.py
+++ LinReg.py
@@ -3,39 +3,50 @@ import numpy as np
 import random
 
 class LinReg:
-    def __init__(self, X, y, learn_method=1, epochs=100):
+    def __init__(self, X, y):
         self.X = X
         self.y = y
-        self.epochs = epochs
-        self.learn_method = learn_method
         self.w = np.array([random.randint(-100, 100) / 100 for _ in range(len(X.columns))])  # Инициализация случайных весов
         self.b = random.randint(-100, 100) / 100  # Случайное смещение
 
     def _lin_model(self, X, w, b):  # Вычисление таргетов по фичам, весам и смещению
         return X.dot(w) + b
 
-    def fit(self, learning_rate):
+
+    def fit(self, learn_method='gradient' , learning_rate=0.05, epochs=1000):
         X = self.X # Фичи
         y = self.y # Таргет
-        epochs = self.epochs # Количество циклов обучения
+        w = self.w # Веса
+        b = self.b # Смещение
 
+        if learn_method == 'gradient':
+            for epoch in range(epochs): # Корректировка весов
 
-        for epoch in range(epochs): # Корректировка весов
+                y_true = np.array(y)
+                y_pred = self._lin_model(X, w, b)
+                b -= learning_rate * (2 / len(X)) * np.sum(y_pred - y_true)
 
-            y_true = np.array(y)
-            y_pred = self._lin_model(X, self.w, self.b)
-            self.b -= learning_rate * (2 / len(X)) * np.sum(y_pred - y_true)
+                error = y_pred - y_true
+                grad = (2 / len(X)) * np.dot(X.T, error)
+                w -= learning_rate * grad
 
-            error = y_pred - y_true
-            grad = (2 / len(X)) * np.dot(X.T, error)
-            self.w -= learning_rate * grad
+            self.w = w
+            self.b = b
 
+        if learn_method == 'analytical':
 
-        return self._lin_model(X, self.w, self.b)
+            X['ones_for_bias'] = [1 for _ in range(len(X))] # Добавляем колонку с единицами для смещения
+            X_transposed = X.T
+            features_corr_matrix = X_transposed.dot(X)
+            inverse_matrix = np.linalg.inv(features_corr_matrix)
+            new_w = inverse_matrix.dot(X_transposed).dot(y)
+
+            self.w = new_w
 
 
     def predict(self, X_test):
         return self._lin_model(X_test, self.w, self.b)
+
 
 def MAE(y_true, y_pred):
     mae = np.mean(abs(y_true - y_pred))
--- main_code.py
+++ main_code.py
@@ -17,7 +17,7 @@ pd.options.display.width = 0
 df = pd.read_excel('bike.xlsx')
 
 # Разведочный анализ, графики
-EDA(df)
+# EDA(df)
 
 X = df.drop('cnt', axis=1)
 y = df['cnt']
@@ -26,15 +26,8 @@ y = df['cnt']
 X = one_hot(X, [0, 1, 2, 3, 4, 5])
 
 # Обучение модели
-model = LinReg(X, y, learn_method=1, epochs=1000) # learn_meathod пока не реализован
-
-'''
-for i in range(1, 10):
-    model = LinReg(X, y, learn_method=1, epochs=1000)
-    model.fit(learning_rate=i/100)
-    print(MAE(y, model.predict(X)), i/100) '''
-
-model.fit(learning_rate=0.05)
+model = LinReg(X, y)
+model.fit(learn_method='gradient' ,learning_rate=0.05, epochs=100)
 y_pred = model.predict(X)
 
 # Обучение модели sklearn
