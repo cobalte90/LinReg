560e29c627d2ffcf5b755b7835a34cca0f4e4692
cobalte_analytics <69243075+cobalte90@users.noreply.github.com>
2025-03-28 23:10:33 +0300

    Add files via upload

--- src/models/LinReg.py
+++ src/models/LinReg.py
@@ -4,22 +4,20 @@ import random
 
 
 class LinReg:
-    def __init__(self, X, y):
-        self.X = X
-        self.y = y
+    def __init__(self):
         self.w = None
 
-    def fit(self, learn_method='gradient', learning_rate=0.01, epochs=1000):
+    def fit(self, X, y, learn_method='gradient', learning_rate=0.01, epochs=1000):
 
         # Добавляем колонку с единицами для смещения
-        X = np.c_[np.ones(self.X.shape[0]), self.X]  # Добавляем колонку единиц
+        X = np.c_[np.ones(X.shape[0]), X]  # Добавляем колонку единиц
 
         self.w = np.random.rand(X.shape[1])  # Инициализация весов
 
         if learn_method == 'gradient':
-            for epoch in range(epochs):
-                y_true = np.array(self.y)
-                y_pred = self._lin_model(X, self.w)
+            for eph in range(epochs):
+                y_true = np.array(y)
+                y_pred = self._linear_model(X, self.w)
 
                 error = y_pred - y_true
                 grad = (2 / len(X)) * np.dot(X.T, error)
@@ -29,14 +27,42 @@ class LinReg:
             X_transposed = X.T
             features_corr_matrix = X_transposed.dot(X)
             inverse_matrix = np.linalg.inv(features_corr_matrix)
-            new_w = inverse_matrix.dot(X_transposed).dot(self.y)
+            new_w = inverse_matrix.dot(X_transposed).dot(y)
 
             self.w = new_w
 
     def predict(self, X_test):
         # Добавляем колонку с единицами для смещения
         X_test = np.c_[np.ones(X_test.shape[0]), X_test]
-        return self._lin_model(X_test, self.w)
+        return self._linear_model(X_test, self.w)
 
-    def _lin_model(self, X, w):
+    def _linear_model(self, X, w):
         return X.dot(w)
+
+class Ridge:
+    def __init__(self):
+        self.w = None
+
+    def fit(self, X, y, learn_method='gradient', regul_coef=0.01, learning_rate=0.01, epochs=1000):
+        
+        self.w = np.random.rand(X.shape[1] + 1)
+
+        if learn_method == 'gradient':
+            X = np.c_[np.ones(X.shape[0]), X]
+            for eph in range(epochs):
+                y_true = np.array(y)
+                y_pred = self._linear_model(X, self.w)
+                error = y_pred - y_true
+                grad = (2 / X.shape[0]) * (X.T @ error) + (2 * regul_coef * self.w)
+                self.w -= learning_rate * grad
+        
+    def predict(self, X_test):
+        # Добавляем колонку с единицами для смещения
+        X_test = np.c_[np.ones(X_test.shape[0]), X_test]
+        return self._linear_model(X_test, self.w)
+    
+    def _linear_model(self, X, w):
+        return X @ w
+    
+
+
--- src/models/LogReg.py
+++ src/models/LogReg.py
@@ -37,36 +37,3 @@ class LogReg:
         y_pred = self.sigmoid(self.linear_model(X))
         return np.array([ 1 if i >= 0.5 else 0 for i in y_pred])
 
-
-
-# Тестирование модели
-
-X = pd.read_csv('src/training_mush.csv')
-y = X['class']
-X = X.drop('class', axis=1)
-
-y_true = np.array(pd.read_csv('src/testing_y_mush.csv')['class'])
-X_test = pd.read_csv('src/testing_mush.csv')
-
-model = LogReg()
-model.fit(X, y)
-y_pred = model.predict(X_test)
-
-
-# Расчёт метрик
-from metrics import *
-clfm = ClassificationMetrics()
-
-my_acc = clfm.my_accuracy_score(y_true, y_pred)
-my_precision = clfm.my_precision_score(y_true, y_pred)
-my_recall = clfm.my_recall_score(y_true, y_pred)
-my_f1 = clfm.my_f1_score(y_true, y_pred)
-
-# Сравнение моих метрик и метрик sklearn
-res_dict = {
-    'Название метрики' : ['Accuracy','Precision', 'Recall', 'F1_score'],
-    'Мои значения' : [my_acc, my_precision, my_recall, my_f1],
-    'Значения sklearn' : [accuracy_score(y_true, y_pred), precision_score(y_true, y_pred), recall_score(y_true, y_pred), f1_score(y_true, y_pred)]
-}
-result = pd.DataFrame(res_dict)
-print(result)
